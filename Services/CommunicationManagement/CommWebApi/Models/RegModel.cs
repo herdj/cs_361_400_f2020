9e0745180affc1f1b7e509946cd0238955aa7b34
Lawrence <cramelaw@oregonstate.edu>
2020-10-25 12:39:51 -0600

    Added Comms Container

---
+++ Services/CommunicationManagement/CommWebApi/Models/RegModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CommWebApi
+{
+    public class Application
+    {
+        public string first_name {get; set;}
+        public string last_name {get; set;}
+        public string email_address {get; set;}
+    }
+}
