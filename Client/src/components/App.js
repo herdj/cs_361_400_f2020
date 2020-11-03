e212b09da9ccc2e737d0f38ec4cab64b0832dbb4
Joel Herd <joel@cognitiv.ai>
2020-11-03 14:00:16 -0800

    updates

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router,
 import Header from './Header';
 import EditProfile from './EditProfile';
 import HomePage from './HomePage';
-import Invite from './Invite';
+//import Invite from './Invite';
 
 function App() {
   return (
@@ -13,7 +13,6 @@ function App() {
         <Header />
           <Route path="/user-profile/edit" exact component={EditProfile} />
           <Route path="/" exact component={HomePage} />
-          <Route path="/Invite" exact component={Invite} />
       </div>
     </Router>
   );
