dddd5a3660f7007c8b053306c146248696c31e53
joel-cognitiv <59585248+joel-cognitiv@users.noreply.github.com>
2020-11-03 22:08:55 -0800

    Filter Options on Search Page (#22)
    * updates
    
    * added filter based search

--- Client/src/components/App.js
+++ Client/src/components/App.js
@@ -5,6 +5,7 @@ import Header from './Header';
 import EditProfile from './EditProfile';
 import HomePage from './HomePage';
 import Invite from './Invite';
+import Search from './Search';
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
         <Header />
           <Route path="/user-profile/edit" exact component={EditProfile} />
           <Route path="/" exact component={HomePage} />
-          <Route path="/Invite" exact component={Invite} />
+          <Route path="/invite" exact component={Invite} />
+          <Route path="/search" exact component={Search} />
       </div>
     </Router>
   );
